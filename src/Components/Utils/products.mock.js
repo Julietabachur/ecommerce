1fcc36d77133ea935269605675141a1380a64655
Julieta Bachur <julietabachur@hotmail.com>
2022-07-31 17:55:54 -0300

    arreglo path de las fotos

--- src/Components/Utils/products.mock.js
+++ src/Components/Utils/products.mock.js
@@ -3,10 +3,10 @@ const products = [
         id:4,
         title: "Brownie",
         price: 2500,
-        image: 'brownie.jpg',
-        image1: 'brownie1.PNG',
-        image2: 'brownie2.PNG',
-        image3: 'brownie3.jpg',
+        image: 'img/brownie.jpg',
+        image1: 'img/brownie1.PNG',
+        image2: 'img/brownie2.PNG',
+        image3: 'img/brownie3.jpg',
         stock: 10
     }
 ]
