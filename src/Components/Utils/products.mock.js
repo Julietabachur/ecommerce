1cc75ad59ad4380a0116b11f84d41d81af86f593
Julieta Bachur Cicconi <julietabachur@hotmail.com>
2022-08-02 12:15:30 -0300

    agrego fotos y productos al mock

--- src/Components/Utils/products.mock.js
+++ src/Components/Utils/products.mock.js
@@ -1,13 +1,212 @@
 const products = [
     {
-        id:4,
+        id:1,
+        title: "Cheesecake",
+        price: 4000,
+        image: 'img/cheesecake.jpg',
+        image1: 'img/cheesecake1.jpg',
+        image2: 'img/cheesecake2.jpg',
+        image3: 'img/cheesecake3.jpg',
+        stock: 10,
+        categoria: "torta"
+    },
+    {
+        id:2,
         title: "Brownie",
-        price: 2500,
+        price: 3500,
         image: 'img/brownie.jpg',
         image1: 'img/brownie1.PNG',
         image2: 'img/brownie2.PNG',
         image3: 'img/brownie3.jpg',
-        stock: 10
+        stock: 10,
+        categoria: "torta"
+    },
+    {
+        id:3,
+        title: "Tiramissu",
+        price: 3500,
+        image: 'img/tiramissu.jpg',
+        image1: 'img/tiramissu1.jpg',
+        image2: 'img/tiramissu2.jpg',
+        image3: false,
+        stock: 10,
+        categoria: "torta"
+    },
+    {
+        id:4,
+        title: "Alfajor Chocolate",
+        price: 100,
+        image: 'img/choco1.jpg',
+        image1: false,
+        image2: false,
+        image3: false,
+        stock: 10,
+        categoria: "alfajor"
+    },
+    {
+        id:5,
+        title: "Alfajor Maicena",
+        price: 90,
+        image: 'img/maicena.jpg',
+        image1: 'img/maicena1.jpg',
+        image2: false,
+        image3: false,
+        stock: 10,
+        categoria: "alfajor"
+    },
+    {
+        id:6,
+        title: "Alfajor Nuez",
+        price: 150,
+        image: 'img/alfajorNuez.PNG',
+        image1: false,
+        image2: false,
+        image3: false,
+        stock: 10,
+        categoria: "alfajor"
+    },
+    {
+        id:7,
+        title: "Torta Ricota",
+        price: 3000,
+        image: 'img/ricota.jpg',
+        image1: 'img/ricota1.jpg',
+        image2: 'img/ricota2.jpg',
+        image3: false,
+        stock: 10,
+        categoria: "torta"
+    },
+    {
+        id:8,
+        title: "Tartas",
+        price: 2500,
+        image: 'img/tarta3.jpg',
+        image1: 'img/tarta1.jpg',
+        image2: 'img/tarta2.jpg',
+        image3: 'img/pastafrola.jpg',
+        stock: 10,
+        categoria: "torta"
+    },
+    {
+        id:9,
+        title:"Lemon Pie",
+        price: 3000,
+        image: 'img/lemonPie.jpg',
+        image1: 'img/lemonPie1.jpg',
+        image2: 'img/lemonPie2.jpg',
+        image3: false,
+        stock: 10,
+        categoria: "alfajor"
+    }, 
+    {
+        id:10,
+        title:"Cuadrados dulces",
+        price: false,
+        image: 'img/lemonPie.jpg',
+        image1: 'img/lemonPie1.jpg',
+        image2: 'img/lemonPie2.jpg',
+        image3: false,
+        stock: 10,
+        categoria: "alfajor"
+    },
+    {
+        id:11,
+        title:"Chocotorta",
+        price: 4000,
+        image: 'img/chocotorta.jpg',
+        image1: 'img/chocotorta1.jpg',
+        image2: 'img/chocotorta2.jpg',
+        image3: 'img/chocotorta3.jpg',
+        stock: 10,
+        categoria: "torta"
+    },
+    {
+        id:12,
+        title:"Postre Brownie",
+        price: 600,
+        image: 'img/postre-brownie.jpg',
+        image1: false,
+        image2: false,
+        image3: false,
+        stock: 10,
+        categoria: "postre"
+    },
+    {
+        id:13,
+        title:"Postre Oreo",
+        price: 600,
+        image: 'img/postre-oreo.jpg',
+        image1: false,
+        image2: false,
+        image3: false,
+        stock: 10,
+        categoria: "postre"
+    },
+    {
+        id:14,
+        title:"Postre Patagonia",
+        price: 600,
+        image: 'img/postre-patagonia.jpg',
+        image1: false,
+        image2: false,
+        image3: false,
+        stock: 10,
+        categoria: "postre"
+    },
+    {
+        id:15,
+        title:"Cookie Cake",
+        price: 6000,
+        image: 'img/cookieCake.jpg',
+        image1: 'img/cookieCake1.jpg',
+        image2: 'img/cookieCake2.jpg',
+        image3: 'img/cookieCake3.jpg',
+        stock: 10,
+        categoria: "torta"
+    },
+    {
+        id:16,
+        title:"Number/Letter Cake",
+        price: 6000,
+        image: 'img/cookieCake.jpg',
+        image1: 'img/cookieCake1.jpg',
+        image2: 'img/cookieCake2.jpg',
+        image3: 'img/cookieCake3.jpg',
+        stock: 10,
+        categoria: "torta"
+    },
+    {
+        id:17,
+        title:"Number/Letter Cake",
+        price: 6000,
+        image: 'img/numberCake1.jpg',
+        image1: 'img/numberCake.jpg',
+        image2: false,
+        image3: false,
+        stock: 10,
+        categoria: "torta"
+    },
+    {
+        id:18,
+        title:"Scons de queso",
+        price: 400,
+        image: 'img/scons.jpg',
+        image1: false,
+        image2: false,
+        image3: false,
+        stock: 10,
+        categoria: "salado"
+    },
+    {
+        id:19,
+        title:"Pan saborizado",
+        price: 400,
+        image: 'img/pancitos1.jpg',
+        image1: false,
+        image2: false,
+        image3: false,
+        stock: 10,
+        categoria: "salado"
     }
 ]
 
