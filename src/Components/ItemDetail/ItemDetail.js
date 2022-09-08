2a187cae6340f59f620264785139e61108d62bec
Julieta Bachur Cicconi <julietabachur@hotmail.com>
2022-09-08 13:05:40 -0300

    dasdas

--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -12,27 +12,26 @@ const ItemDetail = ({dataProducts}) => {
 
   return (         
         <div key={id} className="d-flex flex-row flex-wrap align-items-center justify-content-around m-5 contenedor-detail">   
-
-          <div id="carouselExampleControls" class="carousel slide carousel-fade w-50 d-flex justify-content-center align-items-center" data-bs-ride="carousel">
-            <div class="carousel-inner d-flex justify-content-center align-items-center">
+          <div id="carouselExampleControls" className="col-6 carousel slide d-flex justify-content-center align-items-center" data-bs-ride="carousel">
+            <div className="carousel-inner">
             { imagenes && 
               imagenes.length > 0 && imagenes.map((image, index) =>(    
-                <div class="carousel-item active ">
-                  <img src={`/${image}`} key={index} className="foto-secundaria d-block w-50 " alt=""/>
+                <div className="carousel-item active ">
+                  <img src={`/${image}`} key={index} className="foto-secundaria d-block img-fluid" alt=""/>
                 </div>
               ))              
             }        
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
             </button>
           </div>                  
-          <div className=" d-flex flex-column align-items-center justify-content-center border border-3 rounded-3 p-5 h-100">
+          <div className="col-6 d-flex flex-column align-items-center justify-content-center border border-3 rounded-3 p-5 h-100">
             <h2 className="display-3 m-2 text-center">{title}</h2>
             <p className="info d-flex justify-content-center lead text-center my-3">{description}</p>
             <div className="d-flex flex-row align-items-center justify-content-around w-50">
